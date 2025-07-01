75d097bbe63d728678362eab979ba6b1fbedf67c
m3d1s4nt4-19 <antonio.medisanta19@gmail.com>
2025-07-01 12:53:46 +0100

    Colocación Naturalezas

--- src/main/java/com/rpgen/pokemon/NatureDatabase.java
+++ src/main/java/com/rpgen/pokemon/NatureDatabase.java
@@ -11,31 +11,36 @@ public class NatureDatabase {
     
     private static void initializeNatures() {
         // Naturalezas que aumentan y disminuyen estadísticas
-        natures.put("hardy", new Nature("hardy", "Fuerte", null, null, "No afecta las estadísticas"));
-        natures.put("lonely", new Nature("lonely", "Huraña", "attack", "defense", "Aumenta el Ataque, disminuye la Defensa"));
-        natures.put("brave", new Nature("brave", "Audaz", "attack", "speed", "Aumenta el Ataque, disminuye la Velocidad"));
-        natures.put("adamant", new Nature("adamant", "Firme", "attack", "special_attack", "Aumenta el Ataque, disminuye el Ataque Especial"));
-        natures.put("naughty", new Nature("naughty", "Pícara", "attack", "special_defense", "Aumenta el Ataque, disminuye la Defensa Especial"));
-        natures.put("bold", new Nature("bold", "Osada", "defense", "attack", "Aumenta la Defensa, disminuye el Ataque"));
-        natures.put("docile", new Nature("docile", "Dócil", null, null, "No afecta las estadísticas"));
-        natures.put("relaxed", new Nature("relaxed", "Plácida", "defense", "speed", "Aumenta la Defensa, disminuye la Velocidad"));
-        natures.put("impish", new Nature("impish", "Agitada", "defense", "special_attack", "Aumenta la Defensa, disminuye el Ataque Especial"));
-        natures.put("lax", new Nature("lax", "Floja", "defense", "special_defense", "Aumenta la Defensa, disminuye la Defensa Especial"));
-        natures.put("timid", new Nature("timid", "Miedosa", "speed", "attack", "Aumenta la Velocidad, disminuye el Ataque"));
-        natures.put("hasty", new Nature("hasty", "Activa", "speed", "defense", "Aumenta la Velocidad, disminuye la Defensa"));
-        natures.put("serious", new Nature("serious", "Seria", null, null, "No afecta las estadísticas"));
-        natures.put("jolly", new Nature("jolly", "Alegre", "speed", "special_attack", "Aumenta la Velocidad, disminuye el Ataque Especial"));
-        natures.put("naive", new Nature("naive", "Ingenua", "speed", "special_defense", "Aumenta la Velocidad, disminuye la Defensa Especial"));
-        natures.put("modest", new Nature("modest", "Modesta", "special_attack", "attack", "Aumenta el Ataque Especial, disminuye el Ataque"));
-        natures.put("mild", new Nature("mild", "Afable", "special_attack", "defense", "Aumenta el Ataque Especial, disminuye la Defensa"));
-        natures.put("quiet", new Nature("quiet", "Tímida", "special_attack", "speed", "Aumenta el Ataque Especial, disminuye la Velocidad"));
-        natures.put("bashful", new Nature("bashful", "Tímida", null, null, "No afecta las estadísticas"));
-        natures.put("rash", new Nature("rash", "Alocada", "special_attack", "special_defense", "Aumenta el Ataque Especial, disminuye la Defensa Especial"));
-        natures.put("calm", new Nature("calm", "Serena", "special_defense", "attack", "Aumenta la Defensa Especial, disminuye el Ataque"));
-        natures.put("gentle", new Nature("gentle", "Amable", "special_defense", "defense", "Aumenta la Defensa Especial, disminuye la Defensa"));
-        natures.put("sassy", new Nature("sassy", "Grosera", "special_defense", "speed", "Aumenta la Defensa Especial, disminuye la Velocidad"));
-        natures.put("careful", new Nature("careful", "Cauta", "special_defense", "special_attack", "Aumenta la Defensa Especial, disminuye el Ataque Especial"));
-        natures.put("quirky", new Nature("quirky", "Rara", null, null, "No afecta las estadísticas"));
+        natures.put("hardy", new Nature("hardy", "Fuerte", null, null, "Neutral"));
+        natures.put("docile", new Nature("docile", "Dócil", null, null, "Neutral"));
+        natures.put("serious", new Nature("serious", "Seria", null, null, "Neutral"));
+        natures.put("bashful", new Nature("bashful", "Tímida", null, null, "Neutral"));
+        natures.put("quirky", new Nature("quirky", "Rara", null, null, "Neutral"));
+
+        natures.put("lonely", new Nature("lonely", "Huraña", "attack", "defense", "Ataque+, Defensa-"));
+        natures.put("brave", new Nature("brave", "Audaz", "attack", "speed", "Ataque+, Velocidad-"));
+        natures.put("adamant", new Nature("adamant", "Firme", "attack", "special_attack", "Ataque+, Ataque Especial-"));
+        natures.put("naughty", new Nature("naughty", "Pícara", "attack", "special_defense", "Ataque+, Defensa Especial-"));
+
+        natures.put("bold", new Nature("bold", "Osada", "defense", "attack", "Defensa+, Ataque-"));
+        natures.put("relaxed", new Nature("relaxed", "Plácida", "defense", "speed", "Defensa+, Velocidad-"));
+        natures.put("impish", new Nature("impish", "Agitada", "defense", "special_attack", "Defensa+, Ataque Especial-"));
+        natures.put("lax", new Nature("lax", "Floja", "defense", "special_defense", "Defensa+, Defensa Especial-"));
+
+        natures.put("modest", new Nature("modest", "Modesta", "special_attack", "attack", "Ataque Especial+, Ataque-"));
+        natures.put("mild", new Nature("mild", "Afable", "special_attack", "defense", "Ataque Especial+, Defensa-"));
+        natures.put("quiet", new Nature("quiet", "Tímida", "special_attack", "speed", "Ataque Especial+, Velocidad-"));
+        natures.put("rash", new Nature("rash", "Alocada", "special_attack", "special_defense", "Ataque Especial+, Defensa Especial-"));
+
+        natures.put("calm", new Nature("calm", "Serena", "special_defense", "attack", "Defensa Especial+, Ataque-"));
+        natures.put("gentle", new Nature("gentle", "Amable", "special_defense", "defense", "Defensa Especial+, Defensa-"));
+        natures.put("sassy", new Nature("sassy", "Grosera", "special_defense", "speed", "Defensa Especial+, Velocidad-"));
+        natures.put("careful", new Nature("careful", "Cauta", "special_defense", "special_attack", "Defensa Especial+, Ataque Especial-"));
+
+        natures.put("timid", new Nature("timid", "Miedosa", "speed", "attack", "Velocidad+, Ataque-"));
+        natures.put("hasty", new Nature("hasty", "Activa", "speed", "defense", "Velocidad+, Defensa-"));
+        natures.put("jolly", new Nature("jolly", "Alegre", "speed", "special_attack", "Velocidad+, Ataque Especial-"));
+        natures.put("naive", new Nature("naive", "Ingenua", "speed", "special_defense", "Velocidad+, Defensa Especial-"));
     }
     
     public static Nature getNature(String id) {
